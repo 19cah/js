bcf3894447860760ef898cd126389c97bd5bf767
Carlos Abraham <abraham@abraham.ml>
2017-10-24 11:26:53 -0400

    Create example2.js

---
+++ javascript/Control Flow/example2.js
@@ -0,0 +1,11 @@
+var stopLight = 'green';
+
+if (stopLight === 'red') {
+  console.log('Stop');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
