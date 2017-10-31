a72b2e665ae126584e2d4f1244a596422575f4ad
Carlos Abraham <abraham@abraham.ml>
2017-10-31 17:55:15 -0400

    Create ifelse.js

---
+++ javascript/3. Types and Operators/ifelse.js
@@ -0,0 +1,14 @@
+var moonPhase = 'solar eclipse';
+
+if (moonPhase === 'full') {
+	console.log('full');
+}
+else if (moonPhase === 'mostly full') {
+	console.log('Arms and legs are getting hairier');
+}
+else if (moonPhase === 'mostly new') {
+	console.log('Back on two feet');
+} else {
+	console.log('Invalid moon phase');
+}
+//Output : Invalid moon phase
