93094ce2fbd000d1a7c094670456ff5edea93179
Carlos Abraham <abraham@abraham.ml>
2017-11-29 00:40:09 -0500

    Update example1.js

--- javascript/5. Functions/example1.js
+++ javascript/5. Functions/example1.js
@@ -4,3 +4,17 @@ function multiplyByThirteen(inputNumber) {
 
 multiplyByThirteen(9);
 // Output: 117
+
+
+
+
+/*other Function using an string */
+
+function takeOrder(topping){
+  console.log('Order: pizza topped with ' + topping);
+}
+var count;
+
+takeOrder('cheese');
+
+
