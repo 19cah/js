de1de671485392c89f8ef7fdf654eb8fe4896d76
Carlos Abraham <abraham@19cah.com>
2017-12-26 01:53:35 -0500

    🍕 Taking orders with functions

---
+++ javascript/5. Functions/examples/pizza.js
@@ -0,0 +1,10 @@
+function takeOrder(topping, crustType){
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+}
+var count;
+
+takeOrder('bacon','thin crust');
+takeOrder('pineapple','cheese crust');
+takeOrder('ham','bold crust');
+
+
